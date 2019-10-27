f7d157b9aaf75d68ff4d88d830130d0f97b9cced
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-27 21:59:56 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -7,7 +7,19 @@ class Patient
   end
   
   def new_appoinment(doctor, date)
-    Appointment.new()
+    Appointment.new(date, self, doctor)
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    self.appointments.collect{|appointment| appointment.doctor}.uniq
   end
   
 end
